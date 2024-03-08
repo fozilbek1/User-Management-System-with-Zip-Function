50f337db30af183c2965e5e6a1886f46c20e1f9f
Fozil <91082400+fozilbek1@users.noreply.github.com>
2024-03-08 16:39:38 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+usernames = ["Dude", "Bro", "Mister"]
+passwords = ("p@ssword", "abc123", "guest")
+login_dates = ["1/1/2021","1/2/2021","1/3/2021"]
+
+users = list(zip(usernames,passwords))
+
+for i in users:
+    print(i)
+
+users = dict(zip(usernames,passwords))
+
+for key,value in users.items():
+    print(key+" : "+value)
+
+users = zip(usernames,passwords,login_dates)
+
+for i in users:
+    print(i)
